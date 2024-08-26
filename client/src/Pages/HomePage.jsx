30378b6cd4a3528aa18681798f00c0b364fc5343
Het Monpara <131437054+Het2518@users.noreply.github.com>
2024-08-26 10:34:23 +0530

    Completed full-stack application

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
             <div className="max-w-screen-lg mx-auto p-8 bg-white rounded-lg shadow-xl">
                 <h1 className="text-3xl font-bold text-center mb-8">Welcome to Our App</h1>
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 md:flex-row flex-col">
                     <Link to="/login"
                           className=" h-fit w-fit bg-white rounded-full p-4 transition duration-300 ease-in-out transform hover:scale-105">
                         <img src={signInImage} alt="Sign In" className="w-full h-full object-cover"/>
